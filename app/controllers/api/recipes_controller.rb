33f084280bc1791d35f8320311b75f6e71a2d224
lgonz053 <lgonz053@gmail.com>
2019-01-18 17:08:05 -0600

    create,show,destroy

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,6 +1,43 @@
 class Api::RecipesController < ApplicationController
-  def one_recipe_action
-    @recipe = Recipe.last
-    render 'one_recipe_view.json.jbuilder'
+  def index
+    @recipes = Recipe.all
+    render 'index.json.jbuilder'
+  end
+
+  def create
+    @recipe = Recipe.new(
+                          title: params[:title],
+                          chef: params[:chef],
+                          ingredients: params[:ingredients],
+                          directions: params[:directions],
+                          prep_time: params[:prep_time]
+                        )
+    @recipe.save
+    render 'show.json.jbuilder'
+  end
+
+  def show
+    recipe_id = params[:id]
+    @recipe = Recipe.find(recipe_id)
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.title = params[:title] || @recipe.title
+    @recipe.chef = params[:chef] || @recipe.chef
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+    @recipe.image_url = params[:image_url] || @recipe.image_url
+
+    @recipe.save
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])re
+    recipe.destroy
+    render json: {message: "Successfully removed recipe"}
   end
 end
