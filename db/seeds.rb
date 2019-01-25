3a2476ec7273c66a07f27f5767733facf31e01fd
lgonz053 <lgonz053@gmail.com>
2019-01-24 18:30:03 -0600

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Recipe.create!([
+  {title: "Raw Egg", chef: "Bad Josh", ingredients: "live chicken, pair of gloves", directions: "place gloves on hands, grip chickens body, squeeze gently", image_url: nil, prep_time: 5},
+  {title: "Hand Sandwich", chef: "Josh", ingredients: "left hand, right hand, lettuce, mayo, avocado", directions: "place left palm up, lay other ingredients on top, finish with right, place hands together, goodnight, Wisconsin!", image_url: nil, prep_time: 15},
+  {title: "Cookies", chef: "Cookie Monster", ingredients: "cookie mix, milk, eggs, love", directions: "cookie, cookie, cookie, darn, should have bought eggs", image_url: nil, prep_time: 380},
+  {title: "ABC Gum", chef: "McGruff the Crime Dog", ingredients: "gum, loafer", directions: "remove gum from shoe, and chew", image_url: nil, prep_time: 30},
+  {title: "Lagsana", chef: "Garfield", ingredients: "tire, old shoe, peppermint toothpaste, noodles", directions: "pile ingredients into pan, heat to 350 degress, wait for fire alarm", image_url: nil, prep_time: 400}
+])
